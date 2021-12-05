24e9ebf4e2c10fe5c57d10468645afa69b1dce58
beoumsu kang <95010758+Kangsaga@users.noreply.github.com>
2021-12-05 13:37:57 +0900

    Add files via upload

---
+++ Exam06for문사용수추출2.java
@@ -0,0 +1,13 @@
+
+public class Exam06for문사용수추출2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i<=10; i++) {
+			if (i % 3 !=0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
