6267a0863074be78e196f9fe655ea82c4fab4e82
beoumsu kang <95010758+Kangsaga@users.noreply.github.com>
2021-12-06 12:05:42 +0900

    Add files via upload

---
+++ Exam03이단.java
@@ -0,0 +1,18 @@
+
+public class Exam03¿Ã¥‹ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num1 = 2;
+		for (int i = 1; i<10; i++) {
+			System.out.print(num1 + "*" + i + "=");
+			System.out.println(num1 * i);
+		}
+		
+		
+		
+		
+	}
+
+}
