8ea26a44fff35c01f7999b9381d55194399c81f9
beoumsu kang <95010758+Kangsaga@users.noreply.github.com>
2021-12-06 12:06:19 +0900

    Add files via upload

---
+++ Exam04이단부터구단.java
@@ -0,0 +1,16 @@
+
+public class Exam04이단부터구단 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int i = 2 ; i<10 ; i++) {
+			System.out.println("");
+			System.out.println("==" + i +"단==");
+			for (int b = 1; b<10;b++) {
+				System.out.println(i + "*" + b + "=" + b*i);
+			}
+		}
+	}
+
+}
