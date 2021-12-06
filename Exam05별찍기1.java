32823547a544d7087ce1bc70f8e2ae5b04a9d57b
beoumsu kang <95010758+Kangsaga@users.noreply.github.com>
2021-12-06 12:07:22 +0900

    Add files via upload

---
+++ Exam05별찍기1.java
@@ -0,0 +1,19 @@
+
+public class Exam05º°Âï±â1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//°øÆ÷ÀÇ º° Âï±â1
+		
+		
+		for (int a = 5; a >= 1; a--) {
+			for (int i = 1; i <= a; i++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+
+	}
+
+}
